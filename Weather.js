1eebfdec04c855bbdd1206388686a2ab7bb1aee9
hyejun-an <61446288+hyejun-an@users.noreply.github.com>
2021-09-11 16:51:50 -0400

    Title and subtitle(not completed)

--- Weather.js
+++ Weather.js
@@ -1,19 +1,101 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import PropTypes from "prop-types";
+import { LinearGradient } from "expo-linear-gradient";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function Weather({temp}) {
+const weatherOptions = {
+    Thunderstorm: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Drizzle: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Rain: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Snow: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Atmosphere: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Clear: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Clouds: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"],
+        title: "Cloudy",
+        subtitle: "It's cloudy!!!"
+    }, 
+    Mist: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Smoke: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Haze: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Dust: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Fog: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Sand: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Dust: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Ash: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Squall: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }, 
+    Tornado: {
+        iconName: "weather-cloudy",
+        gradient: ["#bdc3c7", "#2c3e50"]
+    }
+}
+
+export default function Weather({temp, condition}) {
     return (
-        <View style={styles.container}>
+        <LinearGradient
+            colors={weatherOptions[condition].gradient}
+            style={styles.container}
+        >   
+            <StatusBar barStyle="light-content"/>
             <View style={styles.halfContainer}>
-                <MaterialCommunityIcons name="weather-rainy" size={96} color="black" />
+                <MaterialCommunityIcons 
+                    name= {weatherOptions[condition].iconName}
+                    size={96} 
+                    color="white" />
                 <Text style={styles.temp}>{temp}o</Text>
             </View>
-            <View style={styles.halfContainer}>
-                <Text>{temp}</Text>
+            <View style={{...styles.halfContainer, ...styles.textContainer}}>
+                <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
             </View>
-        </View>
+        </LinearGradient>
         );
 }
 
@@ -29,11 +111,27 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
     temp: {
-        fontSize: 42
+        fontSize: 42,
+        color: "white"
     },
     halfContainer: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
+    },
+    title: {
+        color: "white",
+        fontSize: 54,
+        fontWeight: "300",
+        marginBottom: 10
+    },
+    subtitle: {
+        fontSize: 24,
+        fontWeight: "600",
+        color: "white"
+    },
+    textContainer: {
+        paddingHorizontal: 20,
+        alignItems: "flex-start"
     }
 })
